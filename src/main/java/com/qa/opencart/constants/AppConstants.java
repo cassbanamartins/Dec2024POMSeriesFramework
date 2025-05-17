f01148826341249f01f92fc48868f22a922bd587
Adeyeye Martins <martins.adeyeye@cassbana.com>
2025-05-17 14:11:15 +0100

    updating Login title

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -6,7 +6,7 @@ public class AppConstants {
     public static final int DEFAULT_TIMEOUT = 5;
     public static final int MEDIUM_DEFAULT_TIMEOUT = 10;
     public static final int LONG_DEFAULT_TIMEOUT = 15;
-    public static final String LOGIN_PAGE_TITLE = "Account Login11";
+    public static final String LOGIN_PAGE_TITLE = "Account Login";
     public static final String HOME_PAGE_TITLE = "My Account";
     public static final String LOGIN_PAGE_FRACTIONAL_URL = "route=account/login";
     public static final String HOME_PAGE_FRACTIONAL_URL = "route=account/account";
